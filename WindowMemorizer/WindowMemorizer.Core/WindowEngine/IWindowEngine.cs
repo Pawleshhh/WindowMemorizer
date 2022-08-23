49c039ce2156e5b854b5db269a3305b25d41b364
Mateusz Pawlikowski <30951232+Pawleshhh@users.noreply.github.com>
2022-08-23 18:17:36 +0200

    Pawleshhh/window memorizer manager (#2)
    * Add IWindowMemorizerManager interface
    
    * Add IWindowEngine interface
    
    * Add IDatabaseEngine interface
    
    Co-authored-by: Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>

---
+++ WindowMemorizer/WindowMemorizer.Core/WindowEngine/IWindowEngine.cs
@@ -0,0 +1,19 @@
+﻿namespace WindowMemorizer.Core
+{
+    public interface IWindowEngine
+    {
+
+        void MoveWindow(nint id, int x, int y);
+
+        void ResizeWindow(nint id, double width, double height);
+
+        nint GetSelectedWindow();
+
+        IEnumerable<nint> GetOpenWindows();
+
+        Point GetWindowPosition(nint id);
+
+        Size GetWindowSize(nint id);
+
+    }
+}
---
+++ WindowMemorizer/WindowMemorizer.Core/WindowMemorizerManager/IWindowMemorizerManager.cs
@@ -0,0 +1,15 @@
+﻿namespace WindowMemorizer.Core
+{
+    public interface IWindowMemorizerManager
+    {
+
+        void MemorizeWindowLayout(IEnumerable<WindowInfo> windowInfos);
+
+        void SetupWindowLayout(IEnumerable<WindowInfo> windowInfos);
+
+        void RemoveMemorizedWindowLayout();
+
+        IEnumerable<WindowInfo> GetMemorizedWindowLayout();
+
+    }
+}

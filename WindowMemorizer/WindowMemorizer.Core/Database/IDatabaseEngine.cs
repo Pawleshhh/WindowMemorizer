0df23f5fca83838d58b09f3186100463c8e3ed25
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-08-23 18:16:26 +0200

    Add IDatabaseEngine interface

---
+++ WindowMemorizer/WindowMemorizer.Core/Database/IDatabaseEngine.cs
@@ -0,0 +1,19 @@
+﻿namespace WindowMemorizer.Core
+{
+    public interface IDatabaseEngine
+    {
+
+        void AddWindow(WindowInfo windowInfo);
+
+        bool RemoveWindow(string title);
+
+        bool RemoveWindow(nint id);
+
+        bool RemoveWindow(WindowInfo windowInfo);
+
+        void SaveWindowLayout(IEnumerable<WindowInfo> windowInfos);
+
+        IEnumerable<WindowInfo> RetrieveWindowLayout();
+
+    }
+}

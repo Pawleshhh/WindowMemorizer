f2cc16c818c94e644097652240696fe50ac30199
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-08-23 18:12:39 +0200

    Add IWindowEngine interface

---
+++ WindowMemorizer/WindowMemorizer.Core/Data/Point.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WindowMemorizer.Core
+{
+    public readonly struct Point : IEquatable<Point>
+    {
+
+        public double X { get; }
+        public double Y { get; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj is Point point)
+            {
+                return Equals(point);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ WindowMemorizer/WindowMemorizer.Core/Data/Size.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WindowMemorizer.Core
+{
+    public readonly struct Size : IEquatable<Size>
+    {
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public Size(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public bool Equals(Size other)
+        {
+            return Width == other.Width && Height == other.Height;
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj is Size point)
+            {
+                return Equals(point);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Width, Height);
+        }
+
+        public override string ToString()
+        {
+            return $"({Width}, {Height})";
+        }
+    }
+}
